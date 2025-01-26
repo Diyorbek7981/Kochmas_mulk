45b798ef51ea34f38e92f9502741aef457fd47f0
Diyorbek7981 <abdurahimov.diyorbek7981@gmail.com>
2025-01-27 00:31:26 +0500

    nfsbohbjhvd

--- config/settings.py
+++ config/settings.py
@@ -101,8 +101,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': NAME,
+        'USER': USER,
+        'PASSWORD': PASSWORD,
+        'HOST': HOST,
+        'PORT': PORT,
     }
 }
 
